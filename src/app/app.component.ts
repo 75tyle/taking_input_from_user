97a88c18262d186e9527bccbea8dfa5d51afc1b3
sanghpal <kamblesanghpal2003@gmail.com>
2023-06-17 21:44:38 +0530

    latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'taking_input_from_user';
+  myform:FormGroup | any;
+username: any;
+user:any
+// template driven form
+onSubmit(contactForm:any){
+  console.log(contactForm.value)
+}
+
+
+// reactiveform
+constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+   this.myform = this.formBuilder.group({
+    username1:['']
+   })
+  }
+
+submitform(){
+  console.log(this.myform.value)
+}
+
+// event binding
+
+onInputChange(value:any){
+  console.log(value.target.value)
+  this.user = value.target.value
+}
+submit3(){
+console.log(this.user)
+}
+
+//twoway binding
+
+usern:string='';
+
 }
